0d92e52966304fd40473e44993f39e9cbf3410a1
cuiken <ken.cuik@gmail.com>
2015-04-23 09:41:45 +0800

    --- adapter ---

--- app/src/main/java/com/stud/criminalintent/app/CrimeListFragment.java
+++ app/src/main/java/com/stud/criminalintent/app/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.stud.criminalintent.app;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -17,5 +18,8 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crime_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(), android.R.layout.simple_list_item_1, mCrimes);
+        setListAdapter(adapter);
     }
 }
