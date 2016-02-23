58fc623b0ad50e1ba4f68765a744f893d74deefc
cuiken <phckren@163.com>
2016-02-23 16:53:51 +0800

    --- 浮动上下文菜单 ---

--- app/src/main/java/com/stud/criminalintent/app/CrimeListFragment.java
+++ app/src/main/java/com/stud/criminalintent/app/CrimeListFragment.java
@@ -61,6 +61,26 @@ public class CrimeListFragment extends ListFragment {
         }
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getActivity().getMenuInflater().inflate(R.menu.crime_list_item_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int position = info.position;
+        CrimeAdapter adapter = (CrimeAdapter) getListAdapter();
+        Crime crime = adapter.getItem(position);
+        switch (item.getItemId()) {
+            case R.id.menu_item_delete_crime:
+                CrimeLab.get(getActivity()).deleteCrime(crime);
+                adapter.notifyDataSetChanged();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
     @TargetApi(11)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -99,6 +119,7 @@ public class CrimeListFragment extends ListFragment {
             }
         }
         ListView listView = (ListView) v.findViewById(android.R.id.list);
+        registerForContextMenu(listView);
         listView.setEmptyView(v.findViewById(android.R.id.empty));
         mNewCrimeButton = (Button) v.findViewById(R.id.new_crime_button);
         mNewCrimeButton.setOnClickListener(new View.OnClickListener() {
