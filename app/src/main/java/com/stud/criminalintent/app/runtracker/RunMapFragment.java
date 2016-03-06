302743bd5cca7112b1f3e7586afa0f35a8d01879
ken.cui <ken.cuik@gmail.com>
2016-03-06 22:13:23 +0800

    --定位--

--- app/src/main/java/com/stud/criminalintent/app/runtracker/RunMapFragment.java
+++ app/src/main/java/com/stud/criminalintent/app/runtracker/RunMapFragment.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.SupportMapFragment;
 
 /**
  * @autor Ken.Cui
  */
-public class RunMapFragment extends SupportMapFragment {
+public class RunMapFragment extends SupportMapFragment implements LocationSource, AMapLocationListener {
     private static final String ARG_RUN_ID = "RUN_ID";
     private AMap mAMap;
+
+    private AMapLocationClient mLocationClient = null;
+    private AMapLocationClientOption mLocationOption = null;
+
+    private OnLocationChangedListener mListener;
 
     public static RunMapFragment newInstance(long runId) {
         Bundle args = new Bundle();
@@ -23,11 +33,68 @@ public class RunMapFragment extends SupportMapFragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = super.onCreateView(inflater, parent, savedInstanceState);
 
         mAMap = getMap();
         mAMap.setMyLocationEnabled(true);
+
+
+        mAMap.setLocationSource(this);// 设置定位监听
+        mAMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+
         return v;
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        System.out.println("-----onLocationChanged");
+        if (mListener != null && aMapLocation != null && aMapLocation.getErrorCode() == 0) {
+            mListener.onLocationChanged(aMapLocation);// 显示系统小蓝点
+            System.out.println("----" + aMapLocation.getLatitude() + "---" + aMapLocation.getLongitude());
+        }
+    }
+
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        System.out.println("-----activate");
+        mListener = onLocationChangedListener;
+        if (mLocationClient == null) {
+            mLocationClient = new AMapLocationClient(getActivity());
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mLocationClient.setLocationListener(this);
+
+            mLocationOption.setNeedAddress(true);
+
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mLocationClient.setLocationOption(mLocationOption);
+            //设置定位间隔,单位毫秒,默认为2000ms
+            mLocationOption.setInterval(60000);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mLocationClient.startLocation();
+        }
+    }
+
+    @Override
+    public void deactivate() {
+        System.out.println("-----deactivate");
+        mListener = null;
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.onDestroy();
+        }
+        mLocationClient = null;
     }
 }
