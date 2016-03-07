5227652369efd808e7bb8655ad5a0bcae64d7fc4
cuiken <phckren@163.com>
2016-03-07 14:02:41 +0800

    --- fix---

--- app/src/main/java/com/stud/criminalintent/app/runtracker/RunMapFragment.java
+++ app/src/main/java/com/stud/criminalintent/app/runtracker/RunMapFragment.java
@@ -4,18 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.LocationSource;
-import com.amap.api.maps2d.SupportMapFragment;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.SupportMapFragment;
+
 
 /**
  * @autor Ken.Cui
  */
-public class RunMapFragment extends SupportMapFragment implements LocationSource, AMapLocationListener {
+public class RunMapFragment extends SupportMapFragment implements LocationSource, AMapLocationListener, CompoundButton.OnCheckedChangeListener {
     private static final String ARG_RUN_ID = "RUN_ID";
     private AMap mAMap;
 
@@ -32,21 +34,35 @@ public class RunMapFragment extends SupportMapFragment implements LocationSource
         return rf;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = super.onCreateView(inflater, parent, savedInstanceState);
 
         mAMap = getMap();
-        mAMap.setMyLocationEnabled(true);
-
 
         mAMap.setLocationSource(this);// 设置定位监听
+        mAMap.setMyLocationEnabled(true);
         mAMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        mAMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+
+        mLocationClient = new AMapLocationClient(getActivity());
+        mLocationClient.setLocationListener(this);
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位监听
+        mLocationClient.setLocationListener(this);
+        mLocationOption.setNeedAddress(true);
+        //设置为高精度定位模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(60000);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+        mLocationClient.startLocation();
 
         return v;
     }
@@ -65,26 +81,7 @@ public class RunMapFragment extends SupportMapFragment implements LocationSource
     public void activate(OnLocationChangedListener onLocationChangedListener) {
         System.out.println("-----activate");
         mListener = onLocationChangedListener;
-        if (mLocationClient == null) {
-            mLocationClient = new AMapLocationClient(getActivity());
-            mLocationOption = new AMapLocationClientOption();
-            //设置定位监听
-            mLocationClient.setLocationListener(this);
 
-            mLocationOption.setNeedAddress(true);
-
-            //设置为高精度定位模式
-            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-            //设置定位参数
-            mLocationClient.setLocationOption(mLocationOption);
-            //设置定位间隔,单位毫秒,默认为2000ms
-            mLocationOption.setInterval(60000);
-            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
-            // 在定位结束后，在合适的生命周期调用onDestroy()方法
-            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
-            mLocationClient.startLocation();
-        }
     }
 
     @Override
@@ -96,5 +93,10 @@ public class RunMapFragment extends SupportMapFragment implements LocationSource
             mLocationClient.onDestroy();
         }
         mLocationClient = null;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
     }
 }
